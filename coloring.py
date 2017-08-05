0f27450a7b367e1f4dda56486fb85a94a25d6d8c
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2017-08-05 13:18:30 -0400

    nea changes
    New color map input inserted,
    manual color selection.

--- coloring.py
+++ coloring.py
@@ -5,7 +5,7 @@ import random as rm
 import numpy as np
 
 # Read image
-im_input = cv2.imread('aztecRound.jpg')
+im_input = cv2.imread('harsha_mandala.png')
 
 # converting it to gray if image is color
 im_gray = cv2.cvtColor(im_input,cv2.COLOR_BGR2GRAY)
@@ -29,14 +29,37 @@ contours, hierarchy = cv2.findContours(im_bw,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMP
 # cnt = contours[0]
 # print (cnt)
 
+# color array for lines color map from image
+lines = np.array([[194,114,1],[26,88,210],[33,175,240],[142,48,123],[48,172,122],[247,189,77],[49,19,169]])
+
 # if you want same color in all contours
 # cv2.drawContours(im_input,contours,-1,(255,255,0),-1)
 
 # changing values of color pixels using randint def of lib rand
+# for i in range(len(contours)):
+# 	cv2.drawContours(im_input,contours,i,(rm.randint(1, 255),
+# 		rm.randint(1, 50),
+# 		rm.randint(1, 255)),-1)
+
+# filling color lines in the countours
+color = lines
+colorCount = 7
+ind = 0
+
+areas = [cv2.contourArea(c) for c in contours]
+max_index = np.argmax(areas)
+print max_index
+cv2.drawContours(im_input,contours,max_index,(255,255,255),-1)
+
 for i in range(len(contours)):
-	cv2.drawContours(im_input,contours,i,(rm.randint(1, 255),
-		rm.randint(1, 50),
-		rm.randint(1, 255)),-1)
+	if(i!=max_index):
+		cv2.drawContours(im_input,contours,i,
+			(color[ind,0],color[ind,1],color[ind,2]),
+			-1)
+		if ind >= colorCount-1:
+			ind = 0
+		else:
+			ind = ind + 1
 
 # count1 = 255
 # count2 = 0
@@ -50,6 +73,8 @@ for i in range(1,h-1):
 
 		if im_bw[i,j] == 0:
 			im_input[i,j,:]=0
+
+cv2.imwrite('coloredImage.png',im_input)
 
 
 while(1):
